5963c933fa042cfb09c9edeafa6886a861d2797c
Catherine Claunch <catherine490@revature.net>
2022-05-24 09:19:20 -0500

    Added code to all four models

--- WhirlingInRagsRestaurantApplication/src/main/java/com/revature/restaurant_application/models/CreditCardData.java
+++ WhirlingInRagsRestaurantApplication/src/main/java/com/revature/restaurant_application/models/CreditCardData.java
@@ -1,4 +1,111 @@
 package com.revature.restaurant_application.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "credit_card")
 public class CreditCardData {
+
+    private String cardNumber;
+    private String cardName;
+
+    private int cvv;
+
+    private String expDate;
+    private  int zipCode;
+    private int creditLimit;
+    private String username;
+
+
+
+    public CreditCardData(String cardNumber, String cardName, int cvv, String expDate, int zipCode, int creditLimit, String username) {
+        super();
+        this.cardNumber = cardNumber;
+        this.cardName = cardName;
+        this.cvv = cvv;
+        this.expDate = expDate;
+        this.zipCode = zipCode;
+        this.creditLimit = creditLimit;
+        this.username = username;
+    }
+
+
+    public CreditCardData() {
+
+    }
+
+    // Getters & Setters
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(int creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String toString() {
+        return "CreditCardData{" +
+                "cardnumber='" + cardNumber + '\'' +
+                ", cardname='" + cardName + '\'' +
+                ", cvv='" + cvv + '\'' +
+                ", expdate='" + expDate + '\'' +
+                ", zipcode='" + zipCode + '\'' +
+                ", creditlimit='" + creditLimit + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+
 }
--- WhirlingInRagsRestaurantApplication/src/main/java/com/revature/restaurant_application/models/CustomerData.java
+++ WhirlingInRagsRestaurantApplication/src/main/java/com/revature/restaurant_application/models/CustomerData.java
@@ -1,4 +1,112 @@
 package com.revature.restaurant_application.models;
 
+
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name = "customer")
 public class CustomerData {
+
+    private String username;
+    private String fname;
+    private String lname;
+
+    private String password;
+
+    private int balance;
+    private boolean isAdmin;
+
+
+
+    public CustomerData(String username, String fname, String lname, String password, int balance, boolean isAdmin) {
+        super();
+        this.username = username;
+        this.fname = fname;
+        this.lname = lname;
+        this.password = password;
+        this.balance = balance;
+        this.isAdmin = isAdmin;
+    }
+
+    public CustomerData(String password){
+        this.password = password;
+    }
+
+    public CustomerData() {
+
+    }
+
+    // Getters & Setters
+    public String getUsername() {
+        return username;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+
+    public String getFname() {
+        return fname;
+    }
+
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "username='" + username + '\'' +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", balance='" + balance + '\'' +
+              //  ", isAdmin='" + isAdmin + '\'' +
+                '}';
+    }
+
+
 }
+
+
+
